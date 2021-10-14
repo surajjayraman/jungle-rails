a886962780042640b8947bdea84708eeeb226d7c
surajjayraman <suraj.jayraman@gmail.com>
2021-10-13 23:51:32 -0400

    Update the Admin Dashboard counts: add links to the product and category admin list/index pages.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.sum(:quantity)
     @category_count = Category.count
+    @categories = Category.all
   end
 end
